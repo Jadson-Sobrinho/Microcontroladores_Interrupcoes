595118ac2b0553f5bdce72f672dd6d3a4310f675
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-02-01 12:58:04 -0300

    Adição do acionamento dos botões para incrementar e decrementar

--- Microcontrolador_Interrupcoes_tarefa.c
+++ Microcontrolador_Interrupcoes_tarefa.c
@@ -23,59 +23,62 @@
 #define GPIO_LED_GREEN 11
 #define GPIO_LED_BLUE 12
 
+//botão de interupção
+const uint button_0 = 5;
+const uint button_1 = 6;
+
 
 // Variáveis globais para controlar o estado do LED e o tempo
-bool led_state = false;
+bool led_on = false;
 int pisca_count = 0;
 unsigned long last_time = 0;
+int blink_count = 0;
+bool animacao_ativa = false; // Estado da animação
 
-// Função para alternar o estado do LED
-void alternar_led() {
-    led_state = !led_state;
-    gpio_put(GPIO_LED_RED, led_state);
+int animacao_atual = 0;  // Começa na animação 0
+const int num_animacoes = 7; // Número total de animações disponíveis
+
+// Callback do temporizador para piscar LED
+bool repeating_timer_callback(struct repeating_timer *t) {
+    led_on = !led_on;  // Alterna o LED
+    gpio_put(GPIO_LED_RED, led_on);
+    return true;  // Mantém o temporizador rodando
 }
 
-// Função para gerenciar o piscar do LED
-void gerenciar_piscar_led(unsigned long current_time) {
-    // 100ms para cada ciclo de acender/apagar o LED
-    const unsigned long intervalo_piscar = 100;
-
-    if (current_time - last_time >= intervalo_piscar) {
-        alternar_led();
-        last_time = current_time;
-        if (led_state == false) {
-            pisca_count++; // Conta somente os ciclos completos
-        }
-
-        // Reinicia o contador após 5 piscadas
-        if (pisca_count >= 5) {
-            pisca_count = 0;
-            sleep_ms(1000); // Intervalo de 1 segundo após 5 piscadas
-        }
-    }
-}
-
+// Função para gerar cores RGB para matriz de LEDs
 uint32_t matrix_rgb(double r, double g, double b) {
-    unsigned char R, G, B;
-    R = r * 255;
-    G = g * 255;
-    B = b * 255;
-    return (G << 24) | (R << 16) | (B << 8);
+    return ((int)(g * 255) << 24) | ((int)(r * 255) << 16) | ((int)(b * 255) << 8);
 }
 
-void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    for (int16_t i = 0; i < NUMERO_DE_LEDS; i++) {
-            valor_led = matrix_rgb(desenho[24 - i], desenho[24 - i], desenho[24 - i]);
-            pio_sm_put_blocking(pio, sm, valor_led);
+// Atualiza os LEDs da matriz
+void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm) {
+    for (int i = 0; i < NUMERO_DE_LEDS; i++) {
+        valor_led = matrix_rgb(desenho[24 - i], desenho[24 - i], desenho[24 - i]);
+        pio_sm_put_blocking(pio, sm, valor_led);
     }
 }
 
-void exibir_animacao(double* animacao[], int num_desenhos, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b, int delay_ms) {
+// Exibe animação caso ativada
+void exibir_animacao(double* animacao[], int num_desenhos, uint32_t valor_led, PIO pio, uint sm) {
+    if (!animacao_ativa) return; // Só roda se estiver ativada
     for (int i = 0; i < num_desenhos; i++) {
-        desenho_pio(animacao[i], valor_led, pio, sm, r, g, b);
-        sleep_ms(delay_ms);
+        desenho_pio(animacao[i], valor_led, pio, sm);
+        sleep_ms(100);
     }
 }
+
+// Interrupção do botão para ativar/desativar animação
+static void gpio_irq_handler(uint gpio, uint32_t events) {
+    if (gpio == button_0) {
+        animacao_atual = (animacao_atual + 1) % num_animacoes; // Avança para a próxima animação
+        animacao_ativa = true; // Ativa a animação
+    } else if (gpio == button_1) {
+        animacao_atual = (animacao_atual - 1 + num_animacoes) % num_animacoes; // Retrocede para a animação anterior
+        animacao_ativa = true; // Ativa a animação
+    }
+    printf("Animação atual: %d\n", animacao_atual);
+}
+
 
 
 double* animacao_0[] = {desenho1, desenho2, desenho3, desenho4, desenho5, desenho6, desenho7, desenho8, desenho9};
@@ -90,11 +93,6 @@ int num_frames = sizeof(animacao_1) / sizeof(animacao_1[0]);
 double* animacao_2[] = {frame0, frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9};
 int num_desenhos2 = sizeof(animacao_2) / sizeof(animacao_2[0]);
 
-/*
-    TODO: ADICIONAR VARIAVEIS DOS FRAMES DE ANIMAÇÃO 3
-
-*/
-
 
 double* animacao_4[] = {desenho1_1, desenho2_2, desenho3_3, desenho4_4, desenho5_5, desenho6_6, desenho7_7, desenho8_8, desenho9_9, desenho10_10, desenho11_11, desenho12_12, desenho13_13, desenho14_14, desenho15_15};
 int num_desenhos_4 = sizeof(animacao_4) / sizeof(animacao_4[0]);
@@ -107,88 +105,37 @@ int num_desenhos_5 = sizeof(animacao_5) / sizeof(animacao_5[0]);
 double* animacao_6[] = {frame1_6, frame2_6, frame3_6, frame4_6, frame5_6, frame6_6, frame7_6, frame8_6, frame9_6};
 int num_desenhos_6 = sizeof(animacao_6) / sizeof(animacao_6[0]);
 
-/*
-    TODO: ADICIONAR VARIAVEIS DOS FRAMES DE ANIMAÇÃO 7
-
-*/
-
 
 double* animacao_8[] = {frame1_8, frame2_8, frame3_8, frame4_8, frame5_8, frame6_8};
 int num_desenhos_8 = sizeof(animacao_8) / sizeof(animacao_8[0]);
 
 
-
-void acionar_animacao_0(uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    exibir_animacao(animacao_0, num_desenhos, valor_led, pio, sm, r, g, b, 100);
-}
-
-
-void acionar_animacao_1(uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-    exibir_animacao(animacao_1, num_frames, valor_led, pio, sm, r, g, b, 100);
-}
-
-/*
-    TODO: Funcão para acionar a animação 2
-*/
-
-void acionar_animacao_2(uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    exibir_animacao(animacao_2, num_desenhos2, valor_led, pio, sm, r, g, b, 100);
-}
-
-/*
-    TODO: Funcão para acionar a animação 3
-
-*/
-
-
-void acionar_animacao_4(uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    exibir_animacao(animacao_4, num_desenhos_4, valor_led, pio, sm, r, g, b, 100);
-}
-
-void acionar_animacao_5(uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    exibir_animacao(animacao_5, num_desenhos_5, valor_led, pio, sm, r, g, b, 600);
-}
-
-
-
-void acionar_animacao_6(uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    exibir_animacao(animacao_6, num_desenhos_6, valor_led, pio, sm, r, g, b, 100);
-}
-
-
-void acionar_animacao_8(uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    exibir_animacao(animacao_8, num_desenhos_8, valor_led, pio, sm, r, g, b, 300);
-}
-
-
-// Estrutura para mapeamento de teclas e ações
-typedef struct {
-    char key;
-    void (*action)(uint32_t, PIO, uint, double, double, double);
-} KeyAction;
-
-KeyAction key_actions[] = {
-    {'0', acionar_animacao_0},
-    {'1', acionar_animacao_1},
-    {'2', acionar_animacao_2},
-    {'4', acionar_animacao_4},
-    {'5', acionar_animacao_5},
-    {'6', acionar_animacao_6},
-    {'8', acionar_animacao_8}
- 
-};
-
-const int num_key_actions = sizeof(key_actions) / sizeof(key_actions[0]);
-
-// Função para processar a tecla pressionada
-void processar_tecla(char key, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b) {
-    for (int i = 0; i < num_key_actions; i++) {
-        if (key_actions[i].key == key) {
-            key_actions[i].action(valor_led, pio, sm, r, g, b);
-            return;
-        }
+void executar_animacao(int animacao_idx, uint32_t valor_led, PIO pio, uint sm) {
+    switch (animacao_idx) {
+        case 0:
+            exibir_animacao(animacao_0, num_desenhos, valor_led, pio, sm);
+            break;
+        case 1:
+            exibir_animacao(animacao_1, num_frames, valor_led, pio, sm);
+            break;
+        case 2:
+            exibir_animacao(animacao_2, num_desenhos2, valor_led, pio, sm);
+            break;
+        case 3:
+            exibir_animacao(animacao_4, num_desenhos_4, valor_led, pio, sm);
+            break;
+        case 4:
+            exibir_animacao(animacao_5, num_desenhos_5, valor_led, pio, sm);
+            break;
+        case 5:
+            exibir_animacao(animacao_6, num_desenhos_6, valor_led, pio, sm);
+            break;
+        case 6:
+            exibir_animacao(animacao_8, num_desenhos_8, valor_led, pio, sm);
+            break;
+        default:
+            printf("Animação inválida\n");
     }
-
 }
 
 int main() {
@@ -210,34 +157,43 @@ int main() {
     gpio_set_dir(GPIO_LED_GREEN, GPIO_OUT);
     gpio_set_dir(GPIO_LED_BLUE, GPIO_OUT);
 
+    //inicializar o botão de interrupção - GPIO5
+    gpio_init(button_0);
+    gpio_set_dir(button_0, GPIO_IN);
+    gpio_pull_up(button_0);
+
+    //inicializar o botão de interrupção - GPIO6
+    gpio_init(button_1);
+    gpio_set_dir(button_1, GPIO_IN);
+    gpio_pull_up(button_1);
+
     uint offset = pio_add_program(pio, &pio_matrix_program);
     uint sm = pio_claim_unused_sm(pio, true);
     pio_matrix_program_init(pio, sm, offset, OUT_PINO);
 
     //Inicializa a matriz de leds desligados
-    desenho_pio(desenho0, valor_led, pio, sm, r, g, b);
+    desenho_pio(desenho0, valor_led, pio, sm);
+
+    // Declaração de uma estrutura de temporizador de repetição.
+    // Esta estrutura armazenará informações sobre o temporizador configurado.
+    struct repeating_timer timer;
+
+    // Configura o temporizador para chamar a função de callback a cada 1 segundo.
+    add_repeating_timer_ms(100, repeating_timer_callback, NULL, &timer);
 
     
+    // Configurar interrupção no botão
+    gpio_set_irq_enabled_with_callback(button_0, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled(button_1, GPIO_IRQ_EDGE_FALL, true);
 
+
+   // Loop principal
     while (true) {
 
-        // Obtém o tempo atual em milissegundos
-        unsigned long current_time = to_ms_since_boot(get_absolute_time());
-
-        // Gerencia o piscar do LED
-        gerenciar_piscar_led(current_time);
-
-        char key = getchar_timeout_us(0); 
-
-
-        if (key != 0) {
-
-            printf("%c\n", key);
-            processar_tecla(key, valor_led, pio, sm, r, g, b);
-
+        if (animacao_ativa) {
+            executar_animacao(animacao_atual, valor_led, pio, sm);
         }
-        sleep_ms(10);
 
-        
+        sleep_ms(10); // Evita uso excessivo da CPU
     }
 }
