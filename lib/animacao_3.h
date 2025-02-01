15221c968232216d452f1938340178ce29c1c731
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-02-01 12:58:38 -0300

    Adição dos arquivos das animações 3 e 7

---
+++ lib/animacao_3.h
@@ -0,0 +1,16 @@
+#ifndef ANIMACAO_3
+#define ANIMACAO_3
+
+extern double frame00[25];
+extern double frame01[25];
+extern double frame02[25];
+extern double frame03[25];
+extern double frame04[25];
+extern double frame05[25];
+extern double frame06[25];
+extern double frame07[25];
+extern double frame08[25];
+extern double frame09[25];
+
+
+#endif
---
+++ lib/animacao_7.h
@@ -0,0 +1,14 @@
+#ifndef ANIMACAO_7 
+#define ANIMACAO_7
+
+extern double frame1_7[25];
+extern double frame2_7[25];
+extern double frame3_7[25];
+extern double frame4_7[25];
+extern double frame5_7[25];
+extern double frame6_7[25];
+extern double frame7_7[25];
+extern double frame8_7[25];
+extern double frame9_7[25];
+
+#endif // ANIMACAO_7
